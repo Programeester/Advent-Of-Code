c883380caf04b0906d7155b0796a6d4d287074d2
programmeester <87316911+Programeester@users.noreply.github.com>
2021-12-02 12:57:35 +0100

    Update part2.py

--- 2015/day2/part2.py
+++ 2015/day2/part2.py
@@ -1,5 +1,5 @@
 def lint_calc(sorted):
-    return sorted[0] * 2 + sorted[1] * 2
+    return sorted[0] * 2 + sorted[1] * 2 + sorted[0]*sorted[1]*sorted[2]
 
 needed_lint = 0
 with open("data.txt", "r") as f:

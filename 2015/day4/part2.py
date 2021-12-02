6e2bca8b958ac49028c4c869e43f6382353a2310
programmeester <87316911+Programeester@users.noreply.github.com>
2021-12-02 20:44:33 +0100

    Create part2.py

---
+++ 2015/day4/part2.py
@@ -0,0 +1,11 @@
+from hashlib import md5
+
+hashed = ""
+count = 0
+
+while not hashed.startswith("000000"):
+    to_hash = "bgvyzdsv" + str(count)
+    hashed = md5(to_hash.encode()).hexdigest()
+    count += 1
+
+print(count - 1)

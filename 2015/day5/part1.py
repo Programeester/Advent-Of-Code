8fc12070f2677231f48e31af5ab143faa69af42a
programmeester <87316911+Programeester@users.noreply.github.com>
2021-12-02 20:54:33 +0100

    Update part1.py

--- 2015/day5/part1.py
+++ 2015/day5/part1.py
@@ -1,1 +1,11 @@
+bad_letters = ["ab", "cd", "pq", "xy"]
+good_letters =["a", "e", "i", "o", "u"]
 
+with open("/home/bigbrain/Documents/python_tutorial/Advent-Of-Code-main/2015/day5/data.txt", "r") as f:
+    words = f.readlines()
+    for word in words:
+        for bad_letter in bad_letters:
+            if bad_letter in word:
+                pass
+        for good_letter in good_letters:
+            if good_letter in word:

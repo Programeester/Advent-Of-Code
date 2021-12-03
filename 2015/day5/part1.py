3be4ea6f33c5a8f05ce665973c341a73be695c82
programmeester <87316911+Programeester@users.noreply.github.com>
2021-12-03 14:14:47 +0100

    Update part1.py

--- 2015/day5/part1.py
+++ 2015/day5/part1.py
@@ -2,4 +2,4 @@ import re
 
 with open("data.txt", "r") as f:
     words = f.readlines()
-    print(len([s for s in words if (re.search(r'([aeiou].*){3,}', s) and re.search(r'(.)\1', s) and not re.search(r'ab|cd|pq|xy', s))]))
+    print(len([word for word in words if (re.search(r'([aeiou].*){3,}', s) and re.search(r'(.)\1', s) and not re.search(r'ab|cd|pq|xy', s))]))

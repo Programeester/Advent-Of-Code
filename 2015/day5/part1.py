e303f2288d47c097154a40f6653fa646647bb76b
programmeester <87316911+Programeester@users.noreply.github.com>
2021-12-03 14:12:35 +0100

    Update part1.py

--- 2015/day5/part1.py
+++ 2015/day5/part1.py
@@ -1,11 +1,5 @@
-bad_letters = ["ab", "cd", "pq", "xy"]
-good_letters =["a", "e", "i", "o", "u"]
+import re
 
-with open("/home/bigbrain/Documents/python_tutorial/Advent-Of-Code-main/2015/day5/data.txt", "r") as f:
+with open("data.txt", "r") as f:
     words = f.readlines()
-    for word in words:
-        for bad_letter in bad_letters:
-            if bad_letter in word:
-                pass
-        for good_letter in good_letters:
-            if good_letter in word:
+    print(len([s for s in words if (re.search(r'([aeiou].*){3,}', s) and re.search(r'(.)\1', s) and not re.search(r'ab|cd|pq|xy', s))]))
